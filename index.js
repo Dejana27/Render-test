892e716b9565561ec6b7169496483cdf4b361299
Dejana Vukcevic <dejanavukcevic@MNEFE24.local>
2025-01-15 15:50:59 +0100

    fourth commit

--- index.js
+++ index.js
@@ -69,23 +69,26 @@ const generateId = () => {
 }
 
 app.post('/api/notes', (request, response) => {
-  const body = request.body
+//   const body = request.body
 
-  if (!body.content) {
-    return response.status(400).json({ 
-      error: 'content missing' 
+//   if (!body.content) {
+//     return response.status(400).json({ 
+//       error: 'content missing' 
+//     })
+//   }
+
+//   const note = {
+//     content: body.content,
+//     important: body.important || false,
+//     id: generateId(),
+//   }
+
+//   notes = notes.concat(note)
+
+//   response.json(note)
+    Note.find({}).then(notes => {
+        response.json(notes)
     })
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    id: generateId(),
-  }
-
-  notes = notes.concat(note)
-
-  response.json(note)
 })
 
 app.get('/api/notes/:id', (request, response) => {
